65ae2e31d1125f1d576f95cec43b1d3e97b31c56
Rohit Bhargava <rohit@kisan.net>
2023-01-13 20:58:36 +0530

    ✨ feat(step3:add platforms): add a standard platform
    used _semiRandomPlatform method to generate starting platform and add it to the game on first run

--- flame-building-doodle-dash/step_03/lib/game/managers/object_manager.dart
+++ flame-building-doodle-dash/step_03/lib/game/managers/object_manager.dart
@@ -26,6 +26,32 @@ class ObjectManager extends Component with HasGameRef<DoodleDash> {
   final List<Platform> _platforms = [];
 
   // Add Platforms: Add onMount method
+  @override
+  void onMount() {
+    super.onMount();
+
+    var currentX = (gameRef.size.x.floor() / 2).toDouble() - 50;
+
+    var currentY =
+        gameRef.size.y - (_rand.nextInt(gameRef.size.y.floor()) / 3) - 50;
+
+    for (var i = 0; i < 9; i++) {
+      if (i != 0) {
+        currentX = _generateNextX(100);
+        currentY = _generateNextY();
+      }
+      _platforms.add(
+        _semiRandomPlatform(
+          Vector2(
+            currentX,
+            currentY,
+          ),
+        ),
+      );
+
+      add(_platforms[i]);
+    }
+  }
 
   // Add Platforms: Add update method
   @override
@@ -53,7 +79,7 @@ class ObjectManager extends Component with HasGameRef<DoodleDash> {
 
       // Powerups: Add call to _maybeAddPowerup();
     }
-    
+
     super.update(dt);
   }
 
